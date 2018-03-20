17124b28b0b537858535f2dafd8ea670a5b7810e
ambsenyestudi <adrifre7@hotmail.com>
2018-03-20 19:53:35 +0100

    Default explossion assamble

---
+++ Assets/Scripts/Monolith/ExplosionBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionBehaviour : MonoBehaviour {
+    private bool isMouseOver;
+    public float destroyExplosionAfterSeconds = 0.5f;
+    public GameObject explosion;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        processClick();
+	}
+
+    private void processClick()
+    {
+        bool isClick = Input.GetMouseButtonDown(0) && isMouseOver;
+        if (isClick)
+        {
+            //Generetate explosion
+            
+            var lastExplosion = Instantiate(explosion, transform.position, transform.rotation);
+            StartCoroutine(KillAfterSomeTime(destroyExplosionAfterSeconds, lastExplosion));
+        }
+    }
+    IEnumerator KillAfterSomeTime(float seconds, GameObject gObj)
+    {
+        yield return new WaitForSeconds(seconds);
+        GameObject.Destroy(gObj);
+    }
+    void OnMouseOver()
+    {
+        isMouseOver = true;
+    }
+    void OnMouseExit()
+    {
+        isMouseOver = false;
+    }
+}
---
+++ Assets/Scripts/Monolith/MaterialChangeBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChangeBehaviour : MonoBehaviour {
+
+    public Material MainMaterial;
+    public Material RollOverMaterial;
+    public Material PressedMaterial;
+    private Renderer _currRenderer;
+    private bool isOver;
+
+	// Use this for initialization
+	void Start () {
+        _currRenderer = GetComponent<Renderer>();
+        if (!isCurrentMaterial(MainMaterial))
+        {
+            _currRenderer.material = MainMaterial;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //This is just a frame (This is an event)
+        if(Input.GetMouseButtonDown(0) && isOver)
+        {
+            if(!isCurrentMaterial(PressedMaterial))
+            {
+                _currRenderer.material = PressedMaterial;
+            }
+        }
+    }
+    //Those are ciclical events
+    void OnMouseOver()
+    {
+        isOver = true;
+
+        if (!isCurrentMaterial(RollOverMaterial))
+        {
+            _currRenderer.material = RollOverMaterial;
+        }
+    }
+    
+    void OnMouseExit()
+    {
+        isOver = false;
+        if (!isCurrentMaterial(MainMaterial))
+        {
+            _currRenderer.material = MainMaterial;
+        }
+    }
+    private bool isCurrentMaterial(Material mat)
+    {
+        return _currRenderer.material == mat;
+    }
+}
